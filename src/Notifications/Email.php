cca84b733c3f2bf05945f825d8940fe86437fdd1
wkoperski <82573476+wkoperski@users.noreply.github.com>
2024-12-11 22:19:42 +0100

    dane do smpt w env

--- src/Notifications/Email.php
+++ src/Notifications/Email.php
@@ -8,25 +8,28 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+
+
 class Email
 {
-    CONST HOST = 'smtp.office365.com';
+
     CONST USERNAME = 'weryfikacja@wielton.com.pl';
-    CONST PASSWORD = '33XX14X1aW';
+
     CONST NAME = 'Weryfikacja Dostawców';
     private PHPMailer $mail;
 
     public function __construct()
     {
+        $env = parse_ini_file('env-dev');
         $this->mail = new PHPMailer(true);
         $this->mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
         $this->mail->isSMTP();                                            //Send using SMTP
-        $this->mail->Host       = self::HOST ;
+        $this->mail->Host       = $env['SMTP_HOST'] ;
         $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $this->mail->CharSet = 'UTF-8';  //not important
         $this->mail->isSMTP(); //important
-        $this->mail->Username   = self::USERNAME;                     //SMTP username
-        $this->mail->Password   = self::PASSWORD;                               //SMTP password
+        $this->mail->Username   = $env['SMTP_USER'];                     //SMTP username
+        $this->mail->Password   = $env['SMTP_PASSWORD'];                               //SMTP password
         $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $this->mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         $this->mail->SMTPSecure = 'tls'; //important
